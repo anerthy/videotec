0db6c1c8886d6be6d78b51ba0d632ce5f557cdc1
anerthy <112020687+anerthy@users.noreply.github.com>
2022-11-10 21:00:47 -0600

    Create VIDEOTEC DICCIONARIO.sql

---
+++ Scripts/VIDEOTEC DICCIONARIO.sql
@@ -0,0 +1,35 @@
+use VIDEOTEC
+go
+select 
+	d.object_id,
+	a.name [table],
+	b.name [column], 
+	c.name [datatype],  
+	b.max_length, 
+	CASE 
+		WHEN b.is_nullable = 0 THEN 'NOT NULL'
+		ELSE 'NULL'
+	END [Allow Nulls],
+	CASE 
+		WHEN b.is_identity = 0 THEN 'NO'
+		ELSE 'SI'
+	END [Identity],	
+	ep.value [Descripcion],
+	f.ForeignKey, 
+	f.ReferenceTableName, 
+	f.ReferenceColumnName
+from sys.tables a   
+	inner join sys.columns b on a.object_id= b.object_id 
+	inner join sys.systypes c on b.system_type_id= c.xtype 
+	inner join sys.objects d on a.object_id= d.object_id 
+	LEFT JOIN sys.extended_properties ep ON d.object_id = ep.major_id AND b.column_Id = ep.minor_id
+	LEFT JOIN (SELECT 
+				f.name AS ForeignKey,
+				OBJECT_NAME(f.parent_object_id) AS TableName,
+				COL_NAME(fc.parent_object_id,fc.parent_column_id) AS ColumnName,
+				OBJECT_NAME (f.referenced_object_id) AS ReferenceTableName,
+				COL_NAME(fc.referenced_object_id,fc.referenced_column_id) AS ReferenceColumnName
+				FROM sys.foreign_keys AS f
+				INNER JOIN sys.foreign_key_columns AS fc ON f.OBJECT_ID = fc.constraint_object_id) 	f ON f.TableName =a.name AND f.ColumnName =b.name
+WHERE a.name <> 'sysdiagrams' 
+ORDER BY a.name,b.column_Id
